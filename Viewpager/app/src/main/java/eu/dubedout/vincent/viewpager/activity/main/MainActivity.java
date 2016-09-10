b6a43f1f961b86d9a25294f49a0bffd463741b25
Vincent Dubedout <vincent.dubedout@gmail.com>
2016-09-10 14:41:35 -0400

    changing text color of Currently displayed text

--- Viewpager/app/src/main/java/eu/dubedout/vincent/viewpager/activity/main/MainActivity.java
+++ Viewpager/app/src/main/java/eu/dubedout/vincent/viewpager/activity/main/MainActivity.java
@@ -27,7 +27,6 @@ public class MainActivity extends AppCompatActivity {
         List<PageData> pageDataList = getPageDataList();
         MainActivityPagerAdapter adapter = new MainActivityPagerAdapter(pageDataList);
         activityMainViewpager.setAdapter(adapter);
-        activityMainViewpager.setOffscreenPageLimit(pageDataList.size() - 1);
     }
 
     private List<PageData> getPageDataList() {
